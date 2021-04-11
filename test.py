b00040e95a50ec323836173dd1a07a0f67875ed9
4111314 <78329746+4111314@users.noreply.github.com>
2021-04-11 21:38:07 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,8 @@
+[Adblock Plus 2.0]
+! title: My AdFilters for websites_1
+! Version: 20210411
+! 本规则是为了屏蔽一些网站，只是借用了去广告的功能
+!本规则用于测试同步
+! Homepage: https://github.com/4111314/My-AdFilters-for-websites
+example.com
+abc.com
